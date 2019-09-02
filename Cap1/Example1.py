353fed1b0a43ab28cad84b1dfd9b1be2715de31b
LeTurtleBoy <cmoreno@ultracom.com.co>
2019-09-02 10:14:38 -0500

    SpecialMethods

--- Cap1/Example1.py
+++ Cap1/Example1.py
@@ -1,39 +1,41 @@
-''' 
+# -*- coding: latin-1 -*-
+
+"""
 Examples by: LeTurtleBoy
 GitHub: https://github.com/LeTurtleBoy
 
 Examples of implementation of chapter 1 of Fluent Python:
     use of special python methods "__methodname__"
 
-Ejemplos de implementaci贸n del capitulo 1 de Fluent Python:
+Ejemplos de implementaci髇 del capitulo 1 de Fluent Python:
     uso de los metodos especiales de python "__methodname__"
-
-'''
+"""
 
 import collections
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
-class FrenchDeck:
+class FrenchDeck():
     ranks = [str(n) for n in range(2, 11)] + list('JQKA')
-    suits = 'Picas Diamante Tr茅bol Coraz贸n'.split() # 'spades diamonds clubs hearts'
+    suits = 'spades hearts diamonds clubs'.split() # 'spades diamonds clubs hearts'
     
-    '''
+    """
     Creamos la clase y con su constructor inicializamos las variables necesarias
     que dan sentido a la case, en este ejemplo se construlle una baraja francesa 
     y se agregan todos los valores posibles en un mazo de cartas.
 
     We create the class and with its constructor we initialize the necessary variables
     that give meaning to the case, in this example a French deck is built
-    and all the possible values 鈥嬧�媋re added in a deck of cards.
-    '''
+    and all the possible values ??are added in a deck of cards.
+    """
+
     def __init__(self): 
         
         self._cards = [Card(rank, suit) for suit in self.suits
         for rank in self.ranks]
 
-    '''
-    implementaci贸n del metodo __len__, con este uso podemos saber que nuestra clase tiene un tama帽o,
-    para cada definici贸n es diferente, en este caso la longitud logica de una baraja de naipes es la cantidad
+    """
+    implementaci髇 del metodo __len__, con este uso podemos saber que nuestra clase tiene un tama駉,
+    para cada definici髇 es diferente, en este caso la longitud logica de una baraja de naipes es la cantidad
     de cartas que posee, por ende se "sobrecarga" el metodo indicando que cuando se use el metodo len(), tendremos
     un resultado logico en nuestra clase.
 
@@ -42,31 +44,32 @@ class FrenchDeck:
     for each definition is different, in this case the logical length of a deck of cards is the amount
     of letters that he owns, therefore the method is "overloaded" indicating that when the len () method is used, 
     we will have a logical result in our class.
-    '''
+    """
     def __len__(self):
         return len(self._cards)
 
-    '''
-    implementaci贸n del metodo __getitem__, con este uso podemos obtener un elemento de nuestra clase,
+    """
+    implementaci髇 del metodo __getitem__, con este uso podemos obtener un elemento de nuestra clase,
     en el contexto del ejemplo lo logico es obtener una de las cartas de la bara y por ende debemos
-    retornar una carta segun su posici贸n.
+    retornar una carta segun su posici髇.
 
     
     implementation of the __getitem__ method, with this use we can obtain an element of our class,
     in the context of the example the logical thing is to obtain one of the cards of the bara and therefore we must
     return a letter according to your position.
-    '''
-    def __getitem__(self, position): 
+    """
+
+    def __getitem__(self, position):
         return self._cards[position]
 
 
 deck = FrenchDeck() # Creamos un objeto baraja, Create a deck object
 
-'''
-Podemos imprimir un elemento dentro de la baraja por su posici贸n asi como una lista iterable de elementos.
+"""
+Podemos imprimir un elemento dentro de la baraja por su posici髇 asi como una lista iterable de elementos.
 
 we can print an element of the deck using his position, and also can get a list of elements in a pythonic way.
-'''
+"""
 
 print(deck[0]) #  imprimimos la primer carta del mazo, print fisrt card of the deck
 print(deck[0::13]) #  imprimimos la primer carta de cada pinta mazo, print the first card of every color in the deck
---
+++ Cap1/Example2.py
@@ -0,0 +1,101 @@
+# -*- coding: latin-1 -*-
+# Special Methods
+
+
+class Polynomial:
+    def __init__(self, coeff):
+        err_message = "Arguments should be dict in the form {Exponent: Coefficient,...}"
+        if type(coeff) is not dict:
+            raise ValueError(err_message)
+        coeff_ret = {}
+        for key in sorted(coeff.keys())[::-1]:
+            if coeff[key] != 0:
+                coeff_ret[key] = coeff[key]
+        self._coeff = coeff_ret
+
+    def __repr__(self):
+        rep = ' + '.join('{}*x^{}'.format(coe, exp)
+                         if coe is not 1
+                         else 'x^{}'.format(exp)
+                         for (exp, coe) in self._coeff.items())
+        rep = rep.replace(" x^0", " 1").replace("*x^0", "").replace("+ -", "- ")
+
+        return rep
+
+    def __abs__(self):
+        for (exp, coeff) in self._coeff.items():
+            self._coeff[exp] = abs(coeff)
+        return self
+
+    def __add__(self, other):
+        result = {}
+        other = other.get_dict()
+        for key in self._coeff.keys():
+            if key in other.keys():
+                result[key] = self._coeff[key] + other[key]
+            else:
+                result[key] = self._coeff[key]
+        for key in other.keys():
+            if key in self._coeff.keys():
+                pass
+            else:
+                result[key] = other[key]
+        return Polynomial(result)
+
+    def __sub__(self, other):
+        result = {}
+        other = other.get_dict()
+        for key in self._coeff.keys():
+            if key in other.keys():
+                result[key] = self._coeff[key] - other[key]
+            else:
+                result[key] = self._coeff[key]
+        for key in other.keys():
+            if key in self._coeff.keys():
+                pass
+            else:
+                result[key] = other[key]
+        return Polynomial(result)
+
+    def __mul__(self, other):
+        if type(other) in [int, float]:
+            for (key, value) in self._coeff.items():
+                self._coeff[key] = other * value
+            return self
+
+        if type(other) is Polynomial:
+            other = other.get_dict()
+            results = []
+            for (key, value) in other.items():
+                temp = {}
+                for (key_2, value_2) in self._coeff.items():
+                    temp[key + key_2] = value * value_2
+                results.append(Polynomial(temp))
+            aux = results[0]
+            for index in range(len(results)-1):
+                aux = aux + results[index+1]
+            return aux
+
+    def get_dict(self):
+        return self._coeff
+
+    def eval(self, x):
+        sol = sum([c*x**e for (e, c) in self._coeff])
+        return sol
+
+
+if __name__ == "__main__":
+    try:
+        base = [1, 2, 4, 5, 7]
+        Pol_bad = Polynomial(base)
+    except Exception as e:
+        print("Error:", type(e), str(e))
+
+    Pol1 = Polynomial({2: 1, 1: 2, 0: 1})
+    Pol2 = Polynomial({2: 1, 1: 2, 0: 1})
+    print("A = ", Pol1)
+    print("B = ", Pol2)
+    print("A + B = ", Pol1 + Pol2)
+    print("A - B = ", Pol1 - Pol2)
+    print("A - abs(B)*2 = ", Pol1 - abs(Pol2)*2)  # 2 * abs(Pol2)
+    print("A * B = ", Pol1*Pol2)
